e38201fea531c7b15a27e002d267b80f5d2b19e2
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-04-18 19:06:22 +0200

    [Add] some more configuration

--- src/app/configuration-items/configuration-item-daterangepicker/configuration-item-daterangepicker.component.ts
+++ src/app/configuration-items/configuration-item-daterangepicker/configuration-item-daterangepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 import { PluginSettings } from '../../services/plugins-config.provider';
 
@@ -10,9 +11,39 @@ import { PluginSettings } from '../../services/plugins-config.provider';
 export class ConfigurationItemDaterangepickerComponent implements OnInit {
   @Input() data: PluginSettings;
 
-  constructor() { }
+  hoveredDate: NgbDate;
+  fromDate: NgbDate;
+  toDate: NgbDate;
+
+  constructor(calendar: NgbCalendar) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
 
   ngOnInit() {
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
   }
 
 }
